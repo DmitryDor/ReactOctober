64fb45ad1ebdd21ea8485b1a88f745017f9d3b4f
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-09-27 10:55:19 +0300

    Lesson2(component)

--- src/App.tsx
+++ src/App.tsx
@@ -4,46 +4,70 @@ import './App.css';
 
 
 function App() {
-    debugger
+    console.log("App rendering")
     return (
         <div>
-            This is App component
-            <Raiting />
-            <Accordion />
+            <AppTitle/>
+            <Raiting/>
+            <Accordion/>
+            <Raiting/>
+
         </div>
     );
 }
 
+function AppTitle() {
+    console.log("Title rendering")
+    return (
+        <div>This is App component</div>
+    )
+}
 
 function Raiting() {
-    debugger
+    console.log("Raiting rendering")
+
     return (
         <div>
-            <Star />
-            <Star />
-            <Star />
-            <Star />
-            <Star />
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </div>
     )
-
 }
 
 function Accordion() {
-    debugger
+    console.log("Accordion rendering ")
+
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
 
-function Star () {
+function AccordionTitle() {
+    console.log("AccordionTitle rendering ")
+    return (
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering ")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
+
+function Star() {
+    console.log("Star rendering ")
     return <div>star</div>
 }
 
