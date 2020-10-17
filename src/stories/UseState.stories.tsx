f63968c1a57337870dfa16859880cdf53df7c0ac
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-10-17 17:54:26 +0300

    Lesson20(useStste!)

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from "react";
+
+
+export default {
+    title: 'useState demo'
+}
+
+
+function generationData() {
+    //difficult counting
+    console.log('generationData')
+    return 1
+}
+
+export const Example1 = () => {
+    console.log('Example1')
+
+    const [counter, setCounter] = useState(generationData)
+
+    const changer = (state: number) => {
+        debugger
+        return state + 1
+    }
+
+
+
+return <>
+    <button onClick={() => setCounter(changer)}>+</button>
+    <>{counter}</>
+</>
+}
